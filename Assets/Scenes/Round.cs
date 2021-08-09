629199c0b9c4fa475a1d96b8bd7ea6b2e0ae759d
Sofia Bzhilyanskaya <sab4633@rit.edu>
2021-08-08 21:30:18 -0400

    Added more pieces and turn system
    Created board pieces vs point pieces. Created a turn system so only non-cuegivers can place thier pieces on the board. Created an End of turn function without implementation. It will cover points and move the point pieces.

--- Assets/Scenes/Round.cs
+++ Assets/Scenes/Round.cs
@@ -14,11 +14,15 @@ public class Round : MonoBehaviour
     int playerCount;
     int round;
     int maxrounds;
+    int turn;
     Vector3 baseCoordinates = new Vector3(-4.17f, 2.41f ,1f);
     float xdiff = 0.483f;
     float ydiff = 0.455f;
     Dictionary<string, int> alph = new Dictionary<string, int>();
-    SpriteRenderer[] playerSprites = new SpriteRenderer[3];
+    SpriteRenderer[] playerSprites = new SpriteRenderer[6];
+    SpriteRenderer[] pointSprites = new SpriteRenderer[3];
+
+    InputField cueinput;
 
 
     CardScript cs;
@@ -26,6 +30,7 @@ public class Round : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        turn = 0;
         alph.Add("A", 0); alph.Add("B", 1); alph.Add("C", 2); alph.Add("D", 3);
         alph.Add("E", 4); alph.Add("F", 5); alph.Add("G", 6); alph.Add("H", 7);
         alph.Add("I", 8); alph.Add("J", 9); alph.Add("L", 10); alph.Add("M", 11);
@@ -41,16 +46,25 @@ public class Round : MonoBehaviour
         turntracker.text = players[currentPlayer]+" prep";
         cs = GameObject.Find("Card").GetComponent<CardScript>();
 
-        playerSprites[0] = GameObject.Find("p1").GetComponent<SpriteRenderer>();
-        playerSprites[1] = GameObject.Find("p2").GetComponent<SpriteRenderer>();
-        playerSprites[2] = GameObject.Find("p3").GetComponent<SpriteRenderer>();
+        pointSprites[0] = GameObject.Find("p1a").GetComponent<SpriteRenderer>();
+        pointSprites[1] = GameObject.Find("p2a").GetComponent<SpriteRenderer>();
+        pointSprites[2] = GameObject.Find("p3a").GetComponent<SpriteRenderer>();
+
+        playerSprites[0] = GameObject.Find("p1b").GetComponent<SpriteRenderer>();
+        playerSprites[1] = GameObject.Find("p2b").GetComponent<SpriteRenderer>();
+        playerSprites[2] = GameObject.Find("p3b").GetComponent<SpriteRenderer>();
+        playerSprites[3] = GameObject.Find("p1c").GetComponent<SpriteRenderer>();
+        playerSprites[4] = GameObject.Find("p2c").GetComponent<SpriteRenderer>();
+        playerSprites[5] = GameObject.Find("p3c").GetComponent<SpriteRenderer>();
+
+        cueinput = GameObject.Find("CueInput").GetComponent<InputField>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Return) && turntracker.text.Contains("give cue"))
+        if (Input.GetKeyDown(KeyCode.Return) && turntracker.text.Contains("cue"))
         {
             nextPlayer();
         }
@@ -63,7 +77,7 @@ public class Round : MonoBehaviour
             var key = cs.Guess(selectedColor);
             if (!key.Equals(""))
             {
-                playerSprites[currentPlayer].transform.position = new Vector3(
+                playerSprites[currentPlayer+(3*turn)].transform.position = new Vector3(
                     baseCoordinates.x+(xdiff* (int.Parse(key.Substring(1))-1)),
                     baseCoordinates.y-(ydiff* alph[key.Substring(0, 1)]),
                     1f);
@@ -83,9 +97,34 @@ public class Round : MonoBehaviour
 
     void nextPlayer()
     {
+       
         currentPlayer++;
         currentPlayer %= playerCount;
-        turntracker.text = players[currentPlayer] + "'s turn";
+        if (cuegiver.text.Contains(players[currentPlayer]))
+        {
+            if(turn == 1)
+            {
+                turntracker.text = "End of round";
+                EndRound();
+            }
+            else
+            {
+                turntracker.text = players[currentPlayer] + " gives second cue";
+                turn++;
+                cueinput.text = "";
+            }
+            
+        }
+        else
+        {
+            turntracker.text = players[currentPlayer] + "'s turn";
+        }
+        
+    }
+
+    void EndRound()
+    {
+
     }
 
 }
